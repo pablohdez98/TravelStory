e2fe11db7c91006a734f32ec58219a83bf256f32
Javier Sintes <javier@secret-source.eu>
2020-04-11 20:03:29 +0100

    Add tripView component

---
+++ src/app/pages/portal/trip-view/trip-view.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TripViewComponent } from './trip-view.component';
+
+describe('TripViewComponent', () => {
+  let component: TripViewComponent;
+  let fixture: ComponentFixture<TripViewComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TripViewComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TripViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/portal/trip-view/trip-view.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-view',
+  templateUrl: './trip-view.component.html',
+  styleUrls: ['./trip-view.component.scss'],
+})
+export class TripViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
