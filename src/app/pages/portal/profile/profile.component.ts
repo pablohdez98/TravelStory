d346ac296837ba065d4af85de47bcbbbdf7ce771
Pablo Hernández <pablohc224@gmail.com>
2020-04-17 21:45:40 +0100

    Add delete trip function

--- src/app/pages/portal/profile/profile.component.ts
+++ src/app/pages/portal/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {UsersService} from '../../../service/users.service';
+import {UserService} from '../../../services/user/user.service';
 import {Router} from '@angular/router';
+import {TripService} from '../../../services/trip/trip.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -8,24 +10,38 @@ import {Router} from '@angular/router';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  protected trips: any[];
-  protected images: any[];
-  constructor(protected serv: UsersService, private router: Router) {
+  private trips: any[];
+  private images: any[];
+  private userName: string;
+  constructor(private userService: UserService, private tripService: TripService, private router: Router,
+              private toastController: ToastController) {
     this.images = [];
   }
 
   ngOnInit() {
-    this.serv.getUser().subscribe( user => {
+    this.userService.getUser().subscribe( user => {
       if (user) {
-        this.serv.getTrips(false, 'idUser', user.id, '==').subscribe(async userTrips => {
+        this.userName = user.name;
+        this.tripService.getTrips(false, 'idUser', user.id, '==').subscribe(async userTrips => {
           this.trips = await Promise.all(userTrips);
         });
       }
     });
   }
 
+  async deleteTrip(idTrip) {
+    await this.tripService.deleteTrip(idTrip);
+    const toast = await this.toastController.create({
+      message: 'Viaje eliminado',
+      color: 'dark',
+      duration: 2000,
+      position: 'top'
+    });
+    await toast.present();
+  }
+
   async logOut() {
-    await this.serv.logOut();
+    await this.userService.logOut();
     localStorage.removeItem('user');
     await this.router.navigate(['logIn']);
   }
