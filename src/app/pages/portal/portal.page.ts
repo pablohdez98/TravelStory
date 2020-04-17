ca6d8245867306f38501fa621cefc550d1c2e37c
Javier Sintes <javier@secret-source.eu>
2020-04-18 00:04:40 +0100

    Active tab color feature

--- src/app/pages/portal/portal.page.ts
+++ src/app/pages/portal/portal.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['portal.page.scss'],
 })
 export class PortalPage {
-
+  public currentTab: string;
   constructor() {}
-
+  changeTab(tabs) {
+    this.currentTab = tabs.getSelected();
+  }
 }
